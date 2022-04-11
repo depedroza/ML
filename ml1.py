37e27a8c5cb750401327b5c853d4d08929a9c40c
Daniel Pedroza <Daniel_Pedroza1@baylor.edu>
2022-04-11 10:13:15 -0500

    finished ml1

--- ml1.py
+++ ml1.py
@@ -1,3 +1,4 @@
+from typing import ChainMap
 from sklearn.datasets import load_digits
 
 digits = load_digits()
@@ -23,6 +24,54 @@ for item in zip(axes.ravel(), digits.images, digits.target):
     axes.set_title(target)
 
 plt.tight_layout()
-plt.show()
+# plt.show()
 # function for zip is to iterate items (lists for example) together instead of with their
 # own individual for loops
+
+from sklearn.model_selection import train_test_split
+
+data_train, data_test, target_train, target_test = train_test_split(
+    digits.data, digits.target, random_state=11
+)
+
+print(data_train.shape)
+print(data_test.shape)
+print(target_train.shape)
+print(target_test.shape)
+
+from sklearn.neighbors import KNeighborsClassifier
+
+knn = KNeighborsClassifier()
+
+knn.fit(X=data_train, y=target_train)
+
+predicted = knn.predict(X=data_test)
+# whole job of the predict is to give a target answer, so that is why we do not need a y
+
+expected = target_test
+
+print(predicted[:20])
+print(expected[:20])
+
+print(format(knn.score(data_test, target_test), ".2%"))
+
+wrong = [(p, e) for (p, e) in zip(predicted, expected) if p != e]
+
+print(wrong)
+
+from sklearn.metrics import confusion_matrix
+
+confusion = confusion_matrix(y_true=expected, y_pred=predicted)
+
+print(confusion)
+
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt2
+
+confusion_df = pd.DataFrame(confusion, index=range(10), columns=range(10))
+
+figure = plt2.figure(figsize=(7, 6))
+axes = sns.heatmap(confusion_df, annot=True, cmap=plt2.cm.nipy_spectral_r)
+
+plt2.show()
